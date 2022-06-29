3e67c16800d7f3d12247bc03e9b4c0224e927c5d
williamhillarnold@yahoo.com <97689293+will9105@users.noreply.github.com>
2022-06-28 23:16:18 -0400

    readme, screenshot and buttton javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,23 @@
-$('#currentDay').text(moment().format('LLL'));
- 
+ $(document).ready(function() {
+
+     $('#currentDay').text(moment().format('dddd, MMMM Do YYYY'));
+  
+     $('.saveBtn').on('click', function(){
+       console.log(this);
+         var plans = $(this).siblings(".description").value();
+         var time = $(this).parent().attr('id');
+
+         localStorage.setItem(plans, time);
+       });
+
+     $("#9am .description").value(localStorage.getItem("9"));
+     $('#10am .description').value(localStorage.getItem('10'));
+     $('#11am .description').value(localStorage.getItem('11'));
+     $('#12pm .description').value(localStorage.getItem('12'));
+     $('#1pm .description').value(localStorage.getItem('1'));
+     $('#2pm .description').value(localStorage.getItem('2'));
+     $('#3pm .description').value(localStorage.getItem('3'));
+     $('#4pm .description').value(localStorage.getItem('4'));
+     $('#5pm .description').value(localStorage.getItem("5"));
+
+ });
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Work_Day_Scheduler
 This is a scheduler app for a typical 9-5 workday 
+T
